ea026518a2f391d07135f2f512325bce2e0fcec9
sunxcal <aysun.caliskan@hs-furtwangen.de>
2019-12-02 15:23:37 +0100

    änderungen

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.set('view engine', 'ejs');
 app.use('/', require('./routes'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
