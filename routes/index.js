530f3b892af6f6f32957ea3996c6a1a105960a44
mertsilva <mert.daglar@hs-furtwangen.de>
2019-12-02 14:10:04 +0100

    welcome ejs gelöscht

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req, res) => res.render('welcome'));
-
+router.get('/', (req, res) => res.render('login'));
 module.exports = router;
