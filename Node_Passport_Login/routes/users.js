c92a135d472a33e0205d5ae6ac843ed32a8db4ff
sunxcal <aysun.caliskan@hs-furtwangen.de>
2019-12-13 11:29:22 +0100

    hallo

--- Node_Passport_Login/routes/users.js
+++ Node_Passport_Login/routes/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 
 // Register Page
+router.get('/login', (req, res) => res.render('login'));
+
 router.get('/register', (req, res) => res.render('register'));
+
+router.get('/startseite', (req, res) => res.render('startseite'));
 
 module.exports = router;
