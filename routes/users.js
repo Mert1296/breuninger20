00a9128f1408814cd1ae9dfee41e7738cac00ed9
sunxcal <58364503+sunxcal@users.noreply.github.com>
2019-12-26 15:53:03 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
 
+
+// Register Page
+router.get('/login', (req, res) => res.render('login'));
+
+router.get('/register', (req, res) => res.render('register'));
+
+router.get('/startseite', (req, res) => res.render('startseite'));
+
+module.exports = router;
