cfa1dee5e46db2d5e3edac725567d92da9ffbd63
sunxcal <aysun.caliskan@hs-furtwangen.de>
2019-12-28 18:09:09 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Node_Passport_Login/view/layout.ejs
    #	Node_Passport_Login/view/login.ejs
    #	Node_Passport_Login/view/register.ejs
    #	Node_Passport_Login/view/startseite.ejs
    #	Node_Passport_Login/views/login.ejs
    #	Node_Passport_Login/views/startseite.ejs
    #	views/login.ejs
    #	views/startseite.ejs

--- Node_Passport_Login/models/user.js
+++ Node_Passport_Login/models/user.js
@@ -4,5 +4,29 @@ const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: True
+    },
+    username: {
+        type: String,
+        required: True
+    },
+    telefon: {
+        type: String,
+        required: True
+    },
+    email: {
+        type: String,
+        required: True
+    },
+    passwort: {
+        type: String,
+        required: True
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-})
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- app.js
+++ app.js
@@ -1,27 +1,63 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
-
-
+/*
 const http = require('http');
 const fs = require('fs');
-
+ */
 
 const app = express();
 
 //DB Config
 const db = require('./Node_Passport_Login/config/keys').MongoURI;
 
-//Connect do Mongo
-mongoose.connect(db, { useNewUrlParser: true})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err=> console.log(err));
+/// Connect to MongoDB
+mongoose
+    .connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
-//EJS
+// EJS
 app.use(expressLayouts);
 app.use('/css', express.static('css') );
 app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Express session
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Routes
 app.use('/', require('./routes/index.js'));
