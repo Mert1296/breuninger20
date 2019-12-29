0c9b9ab7ea05add00c0192d39a01ee4b83d851c7
mertsilva <mert.daglar@hs-furtwangen.de>
2019-12-29 16:18:35 +0100

    Buchungen Mitarbeiteransicht

---
+++ DB/models/buchung_mitarbeiter.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+//Buchung Schema
+var buchungSchema = mongoose.Schema({
+    user:{
+          type: String,
+          required: true
+      },
+    tor:{
+        type: Number,
+        required: true
+    },
+    datum:{
+        type: String,
+        required: true
+    },
+    sendungsstruktur:{
+        type: String,
+        required: true
+    }
+
+});
+
+var buchung = module.exports = mongoose.model('Buchung', buchungSchema);
+
+//Get Buchungen
+module.exports.getBuchungen = function (callback) {
+    buchung.find(callback);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//buchung.js einbinden
+Buchung = require('../DB/models/buchung_mitarbeiter');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -10,10 +12,20 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Startseite
 router.get('/startseite_breuninger', (req, res) => res.render('startseite_breuninger'));
+router.get('/buchungen', (req,res) =>{
+    Buchung.getBuchungen(function (err, buchung) {
+        if(err){
+            throw err;
+        }
+        res.json(buchung);
+    });
+});
+
 router.get('/startseite_spediteur', (req, res) => res.render('startseite_spediteur'));
 
 //Benutzerverwaltung_MA
 router.get ('/Benutzerverwaltung_Mitarbeiter', (req, res) => res.render ('Benutzerverwaltung_Mitarbeiter'));
+
 //New User MA
 router.get ('/neuerUser_MA', (req, res) => res.render ('neuerUser_MA'));
 router.post('/neuerUser_MA', (req, res) =>{
