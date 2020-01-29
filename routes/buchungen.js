3de2c0c71b6bdc9e19eda735b1c9b53f52054c4f
mertsilva <mert.daglar@hs-furtwangen.de>
2020-01-29 22:26:31 +0100

    Buchungsinfo breuni

--- routes/buchungen.js
+++ routes/buchungen.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-// Load User model
-const User = require('../DB/models/User');
+// Load Buchung model
 const Buchung = require('../DB/models/Buchung');
+const User = require('../DB/models/User');
+const Tor = require('../DB/models/Tor');
 const { ensureAuthenticated } = require('../DB/config/auth');
-const bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
 const db = require('../DB/config/keys').MongoURI;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
 
 //Startseite Breuninger
 router.get ('/startseite_breuninger', ensureAuthenticated, (req, res) => {
@@ -18,6 +18,7 @@ router.get ('/startseite_breuninger', ensureAuthenticated, (req, res) => {
             return res.send(err);
 
         res.render('startseite_breuninger',{
+            user: req.user,
             buchungen: buchungen || []
         });
     });
@@ -36,14 +37,27 @@ router.get ('/startseite_spediteur', ensureAuthenticated, (req, res) => {
     });
 });
 
-//Buhchungsübersicht mitarbeiter
+//Buchungsübersicht mitarbeiter
 router.get('/buchungsuebersicht', (req, res) => res.render('buchungsuebersicht'));
+//Buchungsübersicht spedi
+router.get('/neueBuchung_spediteur', (req, res) => res.render('neueBuchung_spediteur'));
 
-//Neue Buchung spediteur
-router.get('/neueBuchung_spediteur', function(req, res, next) {
-    var user = req.user;
-    //you probably also want to pass this to your view
-    res.render('neueBuchung_spediteur', { user: user });
+//Buchuhungsinfo mitarbeiter
+router.post('/zeigBuchung', (req, res) => {
+    //here it is
+    const id = req.body.zeigen;
+    Buchung.findOne({_id: ObjectID(id)}, function (err, buchung) {
+        res.render('buchungsinfo_mitarbeiter', { buchung: buchung });
+    });
+});
+
+//Buchuhungsinfo spediteur
+router.post('/showBuchung', (req, res) => {
+    //here it is
+    const id = req.body.anzeigen1;
+    Buchung.findOne({_id: ObjectID(id)}, function (err, buchung) {
+        res.render('buchungsinfo_spediteur', { buchung: buchung });
+    });
 });
 
 //torauswahl spedi
@@ -99,12 +113,20 @@ router.post('/torsperren',(req,res) =>{
 
 
 
-router.post('/neueBuchung_spediteur',ensureAuthenticated,(req, res) => {
-    const {gate, username, sendungsstruktur, datepicker, timepicker1, timepicker2, sendungen, EUP, EWP, pakete, bemerkung, teile } = req.body;
-    var user = req.user;
+router.post('/neueBuchung_spediteur', (req, res,) => {
+    const {sendungsstruktur, datepicker, timepicker1, timepicker2, sendungen, EUP, EWP, pakete, bemerkung, teile } = req.body;
+    if (errors.length > 0) {
+        User.findOne({username: "spedi2"}, function (err, user) {
+            console.log(JSON.stringify(req.user));
+            if (err) { throw err; }
+            if (user) {
+                res.render('neueBuchung_spediteur', {
+                    gate: user.gate
+                });
+            }
+        });
+    }
     const newBuchung = new Buchung({
-        gate,
-        username,
         sendungsstruktur,
         datepicker,
         timepicker1,
@@ -115,15 +137,45 @@ router.post('/neueBuchung_spediteur',ensureAuthenticated,(req, res) => {
         pakete,
         bemerkung,
         teile,
-
     });
     newBuchung.save()
         .then(buchung =>{
-            res.render('startseite_spediteur')
+            res.send('saved')
         })
         .catch(err=>console.log(err));
     console.log(newBuchung)
 
 });
+
+//Buchung löschen
+router.post('/deleteBuchung',(req,res) =>{
+    var id = req.body.loeschen1;
+    MongoClient.connect(db, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        dbo.collection("buchungs").deleteOne({"_id": ObjectID(id)}, function(err, res) {
+            if (err) throw err;
+            console.log("1 document deleted");
+            db.close();
+        });
+        res.redirect('back');
+    });
+});
+
+//torauswahl spedi
+router.get ('/neueBuchung_spediteur', (req, res) => {
+
+    Buchung.find(function (err, buchungen) {
+        if (err)
+            return res.send(err);
+
+        res.render('neueBuchung_spedieur',{
+            gate: req.user.gate,
+            buchungen: buchungen || []
+        });
+    });
+});
+
+
 
 module.exports = router;
